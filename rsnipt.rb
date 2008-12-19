2fe643eb21d2576814c5b75f475516fe586f634e
Michael <michaelboutros@MichaelsMacBook.local>
2008-12-19 18:03:31 -0500

    fixed added / updating, updated readme

--- rsnipt.rb
+++ rsnipt.rb
@@ -6,7 +6,7 @@ class Snipt
   attr_reader :lexers, :snipts
   
   def initialize(username, password, user_agent_alias = 'Mac FireFox')
-    @agent = WWW::Mechanize.new
+    @agent = WWW::Mechanize.new    
     @agent.user_agent_alias = user_agent_alias
     
     @logged_in = false
@@ -40,7 +40,7 @@ class Snipt
     @logged_in
   end
 
-  def load_snipts(tag = :*)
+  def load_snipts(tag = nil)
     snipts_page = @agent.get("http://www.snipt.net/#{@username}")
     
     snipts_page.search('ul.snipts/li').each do |snipt|
@@ -52,42 +52,59 @@ class Snipt
     end
   end
   
+  def add(snipt)
+    snipt[:id] = '0'
+    
+    snipt[:tags] = snipt[:tags].join(', ')
+    snipt[:public] = snipt[:public].to_s.capitalize
+    
+    snipt.each do |key, value|
+      snipt.delete(key)
+      snipt[key.to_s] = value.strip
+    end
+    
+    begin
+      @agent.post('http://snipt.net/save/', snipt)
+      load_snipts
+      
+      return :successful => true, :message => 'Snipt added successfully.'
+    rescue
+      return :successful => false, :message => 'An unexpected error occured.'
+    end
+  end
+  
   def update(id_or_snipt, *updates)
     id = id_or_snipt.is_a?(Hash) ? id_or_snipt[:id] : id_or_snipt
 
     if updates.nil? || updates.empty?
-      return :success => true
+      return :successful => true
     end
     
     if (snipt = @snipts.find {|snipt| snipt[:id] == id.to_s}).nil?
-      return :success => false, :message => 'Snipt not found.'
+      return :successful => false, :message => 'Snipt not found.'
     end
     
     if updates[0].key?(:id)
-      return :success => false, :message => 'You cannot change the ID of a snipt.'
+      return :successful => false, :message => 'You cannot change the ID of a snipt.'
     end
     
-    # snipts_page = @agent.get("http://www.snipt.net/#{@username}")
-    # snipt_node = snipts_page.at("li.container-#{id}")
-    
     updated_snipt = snipt.update(updates[0])
-    updated_snipt[:tags] = updated_snipt[:tags].join(',+')
+    updated_snipt[:tags] = updated_snipt[:tags].join(', ')
     updated_snipt[:public] = updated_snipt[:public].to_s.capitalize
-    
-    ['description', 'lexer', 'code'].each do |key|
-      updated_snipt[key.to_sym] = updated_snipt[key.to_sym].to_s.gsub(/ /, '+')
-      end
     
     updated_snipt.each do |key, value|
       updated_snipt.delete(key)
       updated_snipt[key.to_s] = value.strip
     end
     
-    puts updated_snipt.inspect
-    
-    update = @agent.post('http://www.snipt.net/save/', updated_snipt.to_json)
-    
-    return :success => true, :message => ''
+    begin
+      update = @agent.post('http://snipt.net/save/', updated_snipt)
+      load_snipts
+      
+      return :successful => true, :message => 'Snipt updated successfully.'
+    rescue
+      return :successful => false, :message => 'An unexpected error occured.'
+    end
   end
 
   class << self
