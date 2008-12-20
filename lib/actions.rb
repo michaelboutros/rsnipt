819a08c364c8c203121116194706c2db6bdd91c8
Michael <michaelboutros@MichaelsMacBook.local>
2008-12-20 10:39:48 -0500

    added documentation for actions.rb

--- lib/actions.rb
+++ lib/actions.rb
@@ -1,4 +1,19 @@
 class Snipt
+  
+  # This method is used to add snipts directly to a user's account through the client instance. 
+  # The following fields are _required_, and if they are not given then the snipt will not be created:
+  # * +code+: The code or command that makes up the body of the snipt.
+  # * +description+: A short description of what this snipt does.
+  # * +tags+: An Array of tags that describe the snipt.
+  # * +public+: A boolean of whether or not the snipt is to be public.
+  # * +lexer+: The language of this snipt. For more information on lexer's and how to use them in the scope
+  #   of this library, look at Snipt#load_lexers.
+  #
+  # On success, #add will simply return:
+  #   {:successfull => true, :message => 'Snipt added successfully.'} 
+  # 
+  # On fail, #add will return more information in the form of a descriptive error message, such as:
+  #   {:successfull => false, :message => 'All fields are required.'}
   def add(snipt)
     snipt[:id] = '0'
    
@@ -24,6 +39,21 @@ class Snipt
     end
   end
     
+  # This method is used to update a snipt directly through the client interface. This method takes:
+  # * +id+ or +snipt+: Either the id of the snipt, or the SniptStruct instance itself.
+  # * +updates+: A hash containing the updated field values. Note that this will completely rewrite each field,
+  #   so if you want to add a tag, you have to pass the entire tags array, plus the new tag. For example:
+  # 
+  #     snipt = client.snipts(:ruby).first
+  #     snipt.tags # => ['ruby', 'library', 'date']
+  # 
+  #     client.update(snipt, :tags => (snipt.tags << 'time'))
+  # 
+  # On success, this method will simply return true and a message:
+  #   {:successfull => true, :message => 'Snipt updated successfully.'}
+  # 
+  # On failure, the method will return false as well as a more descriptive message, such as:
+  #   {:successful => false, :message => 'Snipt not found.'}
   def update(id_or_snipt, *updates)
     id = id_or_snipt.is_a?(Struct) ? id_or_snipt.id : id_or_snipt
 
@@ -58,6 +88,8 @@ class Snipt
     end
   end
 
+  # This method simply destroys the snipt given. Like with update, you can either
+  # pass the snipt's id or the SniptStruct instance.
   def destroy(id_or_snipt)
     id = id_or_snipt.is_a?(Struct) ? id_or_snipt.id : id_or_snipt
   
--- rsnipt.rb
+++ rsnipt.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'json'
 require 'mechanize'
 
-require 'lib/initialize'
-require 'lib/actions'
-require 'lib/snipt_struct'
-require 'lib/snipts'
+path = File.dirname(__FILE__)
+
+require "#{path}/lib/initialize"
+require "#{path}/lib/actions"
+require "#{path}/lib/snipt_struct"
+require "#{path}/lib/snipts"
