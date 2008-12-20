a5379d889bfdddd646c5b01ef4904e8b7977e78c
Michael <michaelboutros@MichaelsMacBook.local>
2008-12-19 21:08:19 -0500

    added examples for using library with a user

---
+++ examples/with_user.rb
@@ -0,0 +1,40 @@
+require 'rsnipt'
+
+client = Snipt.new('username', 'password')
+if client.logged_in?
+  client.snipts # all of the user's snipts
+  # => [SniptStruct, SniptStruct, etc.]
+  
+  client.snipts(:ruby) # all of the user's snipts that include the 'ruby' tag
+  # => [SniptStruct, SniptStruct, etc.]
+  
+  # There are two ways to edit or remove any snipt. Directly on the SniptStruct,
+  # or through the client.
+  snipt = client.snipts(:ruby).first # => SniptStruct
+  
+  # Directly on the object.
+  destroy = snipt.destroy # => { :successful => true|false, :message => String }
+  if destroy[:successfull]
+    # do something
+  else
+    puts destroy[:message]
+    # do something else
+  end
+  
+  # Through the client.
+  update = client.update(snipt.id, :tags => ['ruby', 'gems'], :public => true) 
+  # => { :successful => true|false, :message => String }
+  
+  if update[:successfull]
+    # do something
+  else
+    puts update[:message]
+    # do something else
+  end
+  
+  # Adding a client is equally easy, and can be done through the client. All fields are required.
+  client.add(:description => 'A test API call.', :code => "puts 'Hello from the API!'", :lexer => client.lexers['ruby'], :tags => ['ruby', 'snipts'], :public => true)
+  # => { :successful => true|false, :message => String }
+else
+  puts "Authentication failed."
+end
--- rsnipt.rb
+++ rsnipt.rb
@@ -6,25 +6,3 @@ require 'lib/initialize'
 require 'lib/actions'
 require 'lib/snipt_struct'
 require 'lib/snipts'
-
-Snipt.snipts(:date).each do |snipt|
-  puts "#{snipt.id} - #{snipt.description} by #{snipt.author} (#{snipt.hash.class})"
-  puts "#{snipt.client}"
-    
-  if (update = snipt.update(:tags => ['date', 'time']))[:successful]
-    puts 'Destroyed successfully.'
-  else
-    puts 'Failed: ' + update[:message]
-  end
-end
-
-Snipt.new('michaelboutros', 'he1100').public_snipts(:date).each do |snipt|
-  puts "#{snipt.id} - #{snipt.description} by #{snipt.author} (#{snipt.hash.class})"
-  puts "#{snipt.client}"
-    
-  if (update = snipt.update(:tags => ['date', 'time']))[:successful]
-    puts 'Updated successfully.'
-  else
-    puts 'Failed: ' + update[:message]
-  end
-end
