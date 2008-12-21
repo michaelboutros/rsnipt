e688bbe7cc121245f41dcbbb46ccd740be481884
Michael <michaelboutros@5601hosta145.starwoodbroadband.com>
2008-12-21 16:26:54 -0500

    more CLI features, specifically relating to the user

--- lib/rsnipt/cli.rb
+++ lib/rsnipt/cli.rb
@@ -4,20 +4,20 @@ require 'base64'
 class SniptCLI
   def initialize(command, arguments)
     self.extend(SniptCLIMethods)
-    
-    commands = ['add', 'exec']
-    
+        
     if arguments.last == '--help'
       send(command.to_sym, arguments)
       exit
-    elsif arguments.length == 0 && !commands.include?(command)
+    elsif arguments.length == 0 && !COMMANDS.include?(command)
       send('exec', [command])
+      exit
     else
       send(command.to_sym, arguments)
+      exit
     end
   end
   
-  def attempt_login(ask = false)
+  def attempt_login(ask = false)    
     if File.exists?(credentials_file) && !ask
       username, password = *File.read(credentials_file).split(': ')
       password = Base64.decode64(password)
@@ -25,6 +25,7 @@ class SniptCLI
       @client = Snipt.new(username, password)
       
       unless @client.logged_in?
+        puts 'Stored credentials not authenticated. Enter new credentials:'
         attempt_login(true)
       end
     else
@@ -48,7 +49,7 @@ class SniptCLI
     "#{ENV['HOME']}/.snipt/credentials"
   end
   
-  def set_credential_permissions		
+  def set_credential_permissions
 		FileUtils.chmod 0700, File.dirname(credentials_file)
 		FileUtils.chmod 0600, credentials_file
   end
@@ -61,6 +62,10 @@ class SniptCLI
   	end
   	
   	set_credential_permissions
+  end
+  
+  def destroy_credentials
+    File.delete(credentials_file)
   end
 
   def ask_for_username
@@ -83,39 +88,42 @@ module SniptCLIMethods
   def add(arguments)
     snipt = {}
     
-    OptionParser.new do |option|
-      option.banner = 'usage: snipt add "[code]" [details]'
+    OptionParser.new do |options|
+      options.banner = 'usage: snipt add "[code]" [details]'
       
-      option.on('-d [description]', String, 'A short description of the snipt.') do |description|
+      options.on('-d [description]', String, 'A short description of the snipt.') do |description|
         snipt[:description] = description
       end
       
-      option.on('-t [tag1,tag2]', Array, 'A comma seperated list of tags for this snipt.') do |tags|
+      options.on('-t [tag1,tag2]', Array, 'A comma seperated list of tags for this snipt.') do |tags|
         snipt[:tags] = tags
       end
       
-      option.on('-l [language]', String, "The code\'s lexer, in short or normal form. See Snipt#lexer_for.") do |lexer|
+      options.on('-l [language]', String, "The code\'s lexer, in short or normal form. See Snipt#lexer_for.") do |lexer|
         snipt[:lexer] = (@client.lexer_for(lexer) || lexer)
       end
       
-      option.on('-p', 'Whether or not the snipt is public.') do |_public|
+      options.on('-p', 'Whether or not the snipt is public.') do |_public|
         snipt[:public] = _public.to_s.capitalize
       end
       
-      option.on('--help', 'Show this message.') do 
-        puts option
+      options.on('--help', 'Show this message.') do 
+        puts options
         exit
       end
       
     end.parse!(arguments)
     
-    if (arguments.length / 2) <= 4
+    if arguments.empty?
+      add(['--help'])
+    elsif (arguments.length / 2) <= 4
       puts "You must provide all fields in order to create a new snipt. Run snipt --help for more information on using this command."
       exit
     end
     
     snipt[:code] = arguments.shift
     
+    puts 'Logging in...'
     attempt_login
     
     add_snipt = @client.add(snipt)
@@ -123,17 +131,10 @@ module SniptCLIMethods
   end
   
   def exec(arguments)
-    if arguments.empty?
-      puts 'No description entered. Run snipt exec --help for options.'
-      exit
-    end
-    
     prompt = false
     
     OptionParser.new do |options|
       options.banner = "usage:  snipt exec <description> [options]\n\tsnipt <description> [options]"
-      
-      options.separator ''
       
       options.on('-p', 'Whether or not to prompt the user before running the code.') do |prompt|
         prompt = true
@@ -145,6 +146,12 @@ module SniptCLIMethods
       end
     end.parse!(arguments)
     
+    if arguments.empty?
+      exec(['--help'])
+      exit
+    end
+    
+    puts 'Logging in...'
     attempt_login
     
     snipt = @client.snipts.find {|snipt| snipt.description == arguments.first }
@@ -168,4 +175,31 @@ module SniptCLIMethods
     end
   end
   
+  def user(arguments)
+    OptionParser.new do |options|
+      options.banner = 'usage: snipt user [options]'
+      
+      options.on('--flush', 'Flush the old user and prompt for new credentials.') do
+        puts 'Flushing old credentials...'
+        destroy_credentials
+        
+        puts 'Enter new information: '        
+        create_credentials_for(ask_for_username, ask_for_password)
+      end
+      
+      options.on('--show', 'Show the current stored credentials.') do
+        username, password = *File.read(credentials_file).split(': ')
+        puts "Current user: #{username}"
+      end
+      
+      options.on('--help', 'Show this message.') do
+        puts options
+        exit
+      end
+    end.parse(arguments)
+    
+    if arguments.empty?
+      user(['--show'])
+    end
+  end
 end
